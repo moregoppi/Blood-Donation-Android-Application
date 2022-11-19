65ab481030fb8d41b12a554ad241bffdb98aa8fc
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-11-19 15:26:18 +0530

    4th commit

--- app/src/main/java/com/example/blooddonationapplication/LoginActivity.java
+++ app/src/main/java/com/example/blooddonationapplication/LoginActivity.java
@@ -6,19 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button registertButton;
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registertButton = findViewById(R.id.registertButton);
+        backButton = findViewById(R.id.backButton);
 
-        registertButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SelectRegistrationActivity.class);
